e6aceb785347c6ecf4e1253935d23789e75e6402
DDaniaLL <91683201+DDaniaLL@users.noreply.github.com>
2022-01-17 23:13:32 +0200

    create usertype controller

---
+++ app/Http/Controllers/UsertypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usertype;
+use Illuminate\Http\Request;
+
+class UsertypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Usertype  $usertype
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Usertype $usertype)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Usertype  $usertype
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Usertype $usertype)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Usertype  $usertype
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Usertype $usertype)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Usertype  $usertype
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Usertype $usertype)
+    {
+        //
+    }
+}
