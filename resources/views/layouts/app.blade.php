2ef65c5276e45f2347876992a6cf0703e8a91d8e
DDaniaLL <91683201+DDaniaLL@users.noreply.github.com>
2022-02-19 18:11:06 +0200

    image for the website favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,6 +119,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>HRMS</title>
+  <link rel="icon" href="{{ asset('adminlte/hr.jpg')}}"/>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
