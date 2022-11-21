a485684552c86d8e15f668ae769cd538a397e152
DDaniall <dany.janboura@gmail.com>
2022-11-21 20:41:21 +0300

    removing image from email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{asset('storage/hr360-3-noBG.png')}}" style=" width:150px;height:50px;" class="logo" alt="Logo">
-<!-- {{asset('storage/hr360-3-noBG.png')}}
+<!-- <img src="{{asset('storage/hr360-3-noBG.png')}}" style=" width:150px;height:50px;" class="logo" alt="Logo">
+{{asset('storage/hr360-3-noBG.png')}}
 {{url('/hr360-3-noBG.png')}} -->
 @else
 {{ $slot }}
