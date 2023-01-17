4623decc6faf8fd0ff3a0204b3dbcbd830bcea5e
DDaniall <dany.janboura@gmail.com>
2023-01-17 12:04:45 +0300

    disable okta login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,37 +55,37 @@ class LoginController extends Controller
     }
     public function handleProviderCallback(Request $request)
     {
-        // return 'You are Always Welcome';
-        $user = Socialite::driver('okta')->user();
+        // // return 'You are Always Welcome';
+        // $user = Socialite::driver('okta')->user();
 
-        // $localUser = User::updateOrCreate([
-        //     'email' => $user->email,
-        // ], [
-        //     'name' => $user->name,
-        //     'token' => $user->token,
-        // ]);
-        $localUser = User::where('email', $user->email)->first();
+        // // $localUser = User::updateOrCreate([
+        // //     'email' => $user->email,
+        // // ], [
+        // //     'name' => $user->name,
+        // //     'token' => $user->token,
+        // // ]);
+        // $localUser = User::where('email', $user->email)->first();
 
-        // create a local user with the email and token from Okta
-        if (!$localUser) {
-            $localUser = User::create([
-                'email' => $user->email,
-                'name' => $user->name,
-                'token' => $user->token,
-            ]);
-        } else {
-            // if the user already exists, just update the token:
-            $localUser->token = $user->token;
-            $localUser->save();
-        }
+        // // create a local user with the email and token from Okta
+        // if (!$localUser) {
+        //     $localUser = User::create([
+        //         'email' => $user->email,
+        //         'name' => $user->name,
+        //         'token' => $user->token,
+        //     ]);
+        // } else {
+        //     // if the user already exists, just update the token:
+        //     $localUser->token = $user->token;
+        //     $localUser->save();
+        // }
 
-        try {
-            Auth::login($localUser);
-        } catch (\Throwable$e) {
-            return redirect('/login');
-        }
+        // try {
+        //     Auth::login($localUser);
+        // } catch (\Throwable$e) {
+        //     return redirect('/login');
+        // }
 
-        return redirect('/home');
+        // return redirect('/home');
     }
 
     public function logout()
