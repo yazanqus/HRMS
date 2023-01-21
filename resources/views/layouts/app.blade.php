c0177fb036ecdc862c7e27b5ab2d543204f3557d
DDaniall <dany.janboura@gmail.com>
2023-01-21 22:12:05 +0300

    fixed title for system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>HR360</title>
+  <title>HR 360</title>
   <link rel="icon" href="{{ asset('adminlte/hr.jpg')}}"/>
 
   <!-- Google Font: Source Sans Pro -->
